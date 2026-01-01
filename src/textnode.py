4202201f49b2ec8518866ad29727adc15bacb936
Morgangx <morgangbulawa@gmail.com>
2026-01-01 16:14:47 +0100

    Small change

--- src/textnode.py
+++ src/textnode.py
@@ -8,16 +8,20 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
-class TextNode():
-    def __init__(self, text: str, text_type: TextType, url: str = None):
-        self.text = text
-        self.text_type = text_type
-        self.url = url
-    def __eq__(self, other: TextNode) -> bool:
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
+        self.text: str = text
+        self.text_type: TextType = text_type
+        self.url: str | None = url
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TextNode):
+            return False
         return (
             self.text == other.text and
             self.text_type == other.text_type and
             self.url == other.url
         )
+    
     def __repr__(self) -> str:
-        return f'TextNode({self.text}, {self.text_type}, {self.url})'
+        return f'TextNode({self.text}, {self.text_type.value}, {self.url})'
